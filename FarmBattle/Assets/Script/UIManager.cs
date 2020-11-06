bd779a5dc4a526a01b29cf3fa58fad109e137db2
Cedric CHAUVIN <cedric.chauvin@edu.devinci.fr>
2020-11-06 15:45:53 +0100

    Condition de victoire + ecran final

--- FarmBattle/Assets/Script/UIManager.cs
+++ FarmBattle/Assets/Script/UIManager.cs
@@ -7,9 +7,16 @@ public class UIManager : MonoBehaviour
 {
     private static UIManager instance;
 
+    public int maxScore = 27;
     public Text team1Score;
     public Text team2Score;
+    public RectTransform demoPanel;
+    public Text demoText;
+    public RectTransform repuPanel;
+    public Text repuText;
 
+    private int score1 = 0;
+    private int score2 = 0;
 
     private void Awake()
     {
@@ -19,12 +26,41 @@ public class UIManager : MonoBehaviour
             Destroy(this.gameObject);
     }
 
+    private void Update()
+    {
+        if (score1 >= 27)
+        {
+            repuPanel.gameObject.SetActive(true);
+            repuText.text = score1.ToString() + "/" + score2.ToString();
+            EndGame();
+        }
+        else if (score2 >= 27)
+        {
+            demoPanel.gameObject.SetActive(false);
+            demoText.text = score2.ToString() + "/" + score1.ToString();
+            EndGame();
+        }
+    }
+
+    private void EndGame()
+    {
+        Time.timeScale = 0;
+        team1Score.transform.parent.gameObject.SetActive(false);
+        team2Score.transform.parent.gameObject.SetActive(false);
+    }
+
     public void AddPoint(Player.TEAM team, int point)
     {
         if (team == Player.TEAM.TEAM1)
-            team1Score.text = (int.Parse(team1Score.text) + point).ToString();  
+        {
+            score1 += point;
+            team1Score.text = (score1).ToString();
+        }
         else
-            team2Score.text = (int.Parse(team2Score.text) + point).ToString();
+        {
+            score2 += point;
+            team2Score.text = (score2).ToString();
+        }
     }
 
     static public UIManager GetInstance() => instance;
