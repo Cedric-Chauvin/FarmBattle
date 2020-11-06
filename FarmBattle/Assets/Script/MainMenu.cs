e014e73a494c94a5d9599e070d1a1a19a25fa81a
Thibaut_Guerin <thibaut.guerin@edu.devinci.fr>
2020-11-06 13:59:44 +0100

    Add Main menu scene

---
+++ FarmBattle/Assets/Script/MainMenu.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Rewired;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    public int playerId;
+    public string sceneToLoad;
+
+    private Rewired.Player player;
+
+    private void Awake()
+    {
+        player = ReInput.players.GetPlayer(playerId);
+    }
+
+    private void Update()
+    {
+        if (player.id == playerId && player.GetButtonDown("Action"))
+        {
+            SceneManager.LoadScene(sceneToLoad);
+        }
+    }
+}
